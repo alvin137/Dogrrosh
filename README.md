29a2853aea1d54a2daf96c29af938b45d1357843
alvin137 <alvin137@naver.com>
2018-08-23 20:41:47 +0900

    1.12

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Dogrrosh
+# Dogrrosh[![Build Status](https://travis-ci.org/alvin137/Dogrrosh.svg?branch=master)](https://travis-ci.org/alvin137/Dogrrosh)
 ---
 A simple items mod
--- src/main/java/alvin137/dogrrosh/item/EagleAxe.java
+++ src/main/java/alvin137/dogrrosh/item/EagleAxe.java
@@ -1,5 +1,7 @@
 package alvin137.dogrrosh.item;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
@@ -10,14 +12,24 @@ import alvin137.dogrrosh.DogrroshMaterial;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.World;
 
 public class EagleAxe extends ItemSword implements Modeled<Item>{
 	private String name = "eagleaxe";
-	private String toolClass;
+	//private String toolClass;
 	private final Set<Block> effectiveBlocks;
     protected float efficiencyOnProperMaterial;
     
@@ -30,7 +42,8 @@ public class EagleAxe extends ItemSword implements Modeled<Item>{
 		setUnlocalizedName(Dogrrosh.MODID + "." +  name);
 		setRegistryName(Dogrrosh.MODID, name);
 		setCreativeTab(Dogrrosh.DOGTAB.get());
-		toolClass = "axe";
+		
+		//toolClass = "axe";
 		
 	}
 	 
@@ -40,5 +53,26 @@ public class EagleAxe extends ItemSword implements Modeled<Item>{
 	        return material != Material.WOOD && material != Material.PLANTS && material != Material.VINE ? super.getStrVsBlock(stack, state) : this.efficiencyOnProperMaterial;
 	}
 	
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+    {
+        stack.damageItem(2, attacker);
+        return true;
+    }
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+		double px = playerIn.posX;
+		double py = playerIn.posY;
+		double pz = playerIn.posZ;
+		List<Entity> elist = new ArrayList<Entity>();
+		elist = worldIn.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(px-10, py-5, pz-10, px+10, py+5, pz+10));
+		for(int i=0; i<elist.size();i++) {
+			elist.get(i).motionX = 10;
+			elist.get(i).motionY = 20;
+		}
+        return new ActionResult(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
+    } 
 	
 }
